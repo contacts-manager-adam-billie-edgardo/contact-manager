fe8cbe25296d6e7fa6f8077339b5f35a54ed636c
Edgardo Ortiz Jr <edgardo.ortiz.jr11@gmail.com>
2023-02-22 13:42:28 -0600

    progress submission for main

--- src/main/java/contactManager/ContactManagerApplication.java
+++ src/main/java/contactManager/ContactManagerApplication.java
@@ -10,19 +10,19 @@ public class ContactManagerApplication {
         while(!exitList) {
 
             //print menu to prompt user to choose item from list
-            menuList.displayList();
+            writeMenu();
             int userOptions = Integer.parseInt(input.nextLine());
             switch (userOptions) {
                 case 1:
-//                    contact();
+                    showContacts();
                     break;
                 case 2:
-//                    addContact();
+                    addContact();
                     break;
                 case 3:
-//                    searchContactByName();
+                    searchContactByName();
                 case 4:
-//                    deleteExistingContact();
+                    deleteExistingContact();
                     break;
                 case 5:
                     exitList = true;
@@ -30,9 +30,25 @@ public class ContactManagerApplication {
             }
         }
     }
-    public class menuList{
-        public static void displayList(){
-            System.out.println("menu");
-        }
+
+    private static void deleteExistingContact() {
+        //deleting contacts method
+    }
+
+    private static void searchContactByName() {
+        //search contacts method
+    }
+
+    private static void addContact() {
+        //add contacts method
+    }
+
+    private static void showContacts() {
+        //show contacts
+    }
+
+    public static void writeMenu(){
+
+            System.out.println("Press ");
     }
 }
