fa7d2c64be1651ea211cde69d9ccb3146dc528fe
Billie <billie.dorries28@gmail.com>
2023-02-22 12:10:49 -0600

    created Contact class skeleton

---
+++ src/main/java/contactManager/Contact.java
@@ -0,0 +1,15 @@
+package contactManager;
+
+public class Contact {
+    private String fName;
+    private String lName;
+    private long phoneNum;
+    private String email;
+
+    public Contact(String fName,String lName,long phoneNum,String email){
+        this.fName = fName;
+        this.lName = lName;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+}
